583c15b02e1f7b0631cd4d135e55faad42e1610a
Danila <fdanya0@gmail.com>
2021-01-02 20:48:08 +0300

    create main extractor class and extract method

--- EmailExtractor/Extractor.cs
+++ EmailExtractor/Extractor.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+using static System.String;
+using static System.Char;
+using System.Linq;
 
 namespace EmailExtractor
 {
-    class Extractor
+    public class Extractor
     {
         /// <summary>
         /// using to find emails in message by certain regulars
         /// </summary>
-        List<Regex> Regexes = new RegexProvider().GetRegexes();
+        public List<Regex> Regexes { get; set; } = new RegexProvider().GetFindRegexes();
+        public Regex CheckRegex { get; set; } = new RegexProvider().CheckEmailRegex;
         /// <summary>
         /// search all unique emails in message
         /// </summary>
@@ -20,12 +24,31 @@ namespace EmailExtractor
         public List<string> GetEmailsFromMessage(string message)
         {
             List<string> result = new();
-            if (message is null)
-                return result;
+            if (IsNullOrWhiteSpace(message))
+                return new();
 
+            message = PrepareToExtract(message);
 
+            //collect all emails finded by regexes
+            List<string> potentialEmails = new();
+            foreach (var r in Regexes)
+                foreach (var m in r.Matches(message) as IEnumerable<Match>)
+                    potentialEmails.Add(m.Value);
+            foreach (var s in potentialEmails)
+                result.Add(s);
+            //remove emails in incorrect format
+            foreach (var e in potentialEmails)
+                if (!CheckRegex.IsMatch(e))
+                    result.Remove(e);
 
-            return result;
+            return result.Distinct().ToList();
         }
+
+        string PrepareToExtract(string message) =>
+            new StringBuilder(message)
+             .ReplaceSpecialBySpace(exclude: "!#$%&'*+-/=?^_`@.{|}~")
+             .ReplaceWhiteSpaceAroundAt()
+            .RemoveDuplicateAt()
+             .ToString();
     }
 }
