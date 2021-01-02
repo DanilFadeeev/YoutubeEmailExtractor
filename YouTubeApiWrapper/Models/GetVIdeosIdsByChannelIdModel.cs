8d189f2a73e26c5a4a8d3628992d5728371b57d9
Danila <fdanya0@gmail.com>
2021-01-02 20:40:35 +0300

    add models for api responces

---
+++ YouTubeApiWrapper/Models/GetVIdeosIdsByChannelIdModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeApiWrapper.Models
+{
+    public class GetVIdeosIdsByChannelIdModel
+    {
+        public string kind { get; set; }
+        public string etag { get; set; }
+        public string nextPageToken { get; set; }
+        public string regionCode { get; set; }
+        public PageInfo pageInfo { get; set; }
+        public List<Item> items { get; set; }
+    }
+    public class PageInfo
+    {
+        public int totalResults { get; set; }
+        public int resultPerPage { get; set; }
+    }
+    public class Item
+    {
+        public string kind { get; set; }
+        public string etag { get; set; }
+        public Id id { get; set; }
+    }
+    public class Id
+    {
+        public string kind { get; set; }
+        public string videoId { get; set; }
+    }
+}
