795a1db17191102f28471eb9c5ee7f09b709ded3
Danila <fdanya0@gmail.com>
2021-01-07 16:17:46 +0300

    add video info model

---
+++ YouTubeApiWrapper/Models/VideoInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiWrapper.Models
+{
+    public class VideoInfo
+    {
+        public string Id { get; set; }
+        public string ImageAddress { get; set; }
+        public string Title { get; set; }
+    }
+}
