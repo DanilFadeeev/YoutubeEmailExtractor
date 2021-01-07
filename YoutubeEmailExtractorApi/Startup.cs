4ded220e57419dfb9965ab8929aae92c6031afbe
Danila <fdanya0@gmail.com>
2021-01-07 16:19:59 +0300

    add cors policy

--- YoutubeEmailExtractorApi/Startup.cs
+++ YoutubeEmailExtractorApi/Startup.cs
@@ -1,12 +1,15 @@
+using ApiWrapper.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YoutubeEmailExtractorApi.Models.Interfaces;
 
 namespace YoutubeEmailExtractorApi
 {
@@ -16,24 +19,36 @@ namespace YoutubeEmailExtractorApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSingleton<IApiWrapper, Models.ApiWrapper>();
+            //dictonary to save video info
+            services.AddSingleton<Dictionary<string, ConcurrentBag<VideoInfo>>>();
+            //dictcionary to write emails by video
+            services.AddSingleton<Dictionary<string, ConcurrentBag<string>>>();
+            //for tests
+            services.AddSingleton<ConcurrentBag<string>>();
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Api}/{action=Index}/{id?}");
+
             });
         }
     }
